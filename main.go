603b54ba4ff1df21356c3a180a869d34df076233
Sam Franklin <46005232+sam-fs@users.noreply.github.com>
2019-05-03 13:11:53 -0400

    Added checks for valid SaveAsJson, GCSOnly, and S3Only config combinations (#55)
    * added SaveAsJson config check for BigQuery
    
    * added SaveAsJson and GCSOnly/S3Only config check
    
    * revised bad config msgs

--- main.go
+++ main.go
@@ -392,8 +392,18 @@ func main() {
 		wh = warehouse.NewLocalDisk(conf)
 	case "redshift":
 		wh = warehouse.NewRedshift(conf)
+		if conf.SaveAsJson {
+			if !conf.S3.S3Only {
+				log.Fatalf("Hauser doesn't currently support loading JSON into Redshift.  Ensure SaveAsJson = false in .toml file.")
+			}
+		}
 	case "bigquery":
 		wh = warehouse.NewBigQuery(conf)
+		if conf.SaveAsJson {
+			if !conf.GCS.GCSOnly {
+				log.Fatalf("Hauser doesn't currently support loading JSON into BigQuery.  Ensure SaveAsJson = false in .toml file.")
+			}
+		}
 	default:
 		if len(conf.Warehouse) == 0 {
 			log.Fatal("Warehouse type must be specified in configuration")
