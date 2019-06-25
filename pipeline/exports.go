8877e2074c6217bde766261f409337f5ede7a049
Cam Phillips <cam.phillips22@gmail.com>
2019-06-25 17:36:43 -0400

    removed unused functions

--- pipeline/exports.go
+++ pipeline/exports.go
@@ -2,9 +2,7 @@ package pipeline
 
 import (
 	"compress/gzip"
-	"encoding/csv"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -22,40 +20,29 @@ const (
 	defaultRetryAfterDuration = time.Duration(10) * time.Second
 )
 
+// Record represents a single event in the downloaded event export
 type Record map[string]interface{}
 
+// RecordGroup represents a group of downloaded exports that may contain one or more "bundles" downloaded from FullStory.
 type RecordGroup struct {
 	bundles []fullstory.ExportMeta
 	records []Record
 }
 
+// ExportData contains metadata about a certain export as well as a reader for the raw data.
 type ExportData struct {
 	meta fullstory.ExportMeta
 	src  io.Reader
 }
 
+// SavedExport
 type SavedExport struct {
 	Meta     []fullstory.ExportMeta
 	Filename string
 }
 
-func (r *Record) GetValueStrings(keys []string) []string {
-	values := make([]string, 0, len(keys))
-	for _, key := range keys {
-		values = append(values, fmt.Sprintf("%v", (*r)[key]))
-	}
-	return values
-}
-
-func (d *ExportData) GetReader(format string, headers []string) (io.ReadCloser, error) {
-	switch format {
-	case "csv":
-		return d.GetCSVReader(headers)
-	default:
-		return d.GetRawReader()
-	}
-}
-
+// GetRecords translates a raw data export download into an array of Records. This includes unzipping and decoding the
+// raw JSON.
 func (d *ExportData) GetRecords() ([]Record, error) {
 	stream, err := gzip.NewReader(d.src)
 	if err != nil {
@@ -85,45 +72,6 @@ func (d *ExportData) GetRecords() ([]Record, error) {
 	}
 
 	return recs, nil
-}
-
-func (d *ExportData) GetCSVReader(headers []string) (io.ReadCloser, error) {
-	stream, err := gzip.NewReader(d.src)
-	if err != nil {
-		log.Print(err)
-		return nil, err
-	}
-	piper, pipew := io.Pipe()
-	csvWriter := csv.NewWriter(pipew)
-
-	go func() {
-		decoder := json.NewDecoder(stream)
-		decoder.UseNumber()
-
-		// skip array open delimiter
-		if _, err := decoder.Token(); err != nil {
-			log.Printf("Failed json decode of array open token: %s", err)
-			return
-		}
-
-		for decoder.More() {
-			var r Record
-			decoder.Decode(&r)
-			csvWriter.Write(r.GetValueStrings(headers))
-		}
-
-		if _, err := decoder.Token(); err != nil {
-			log.Fatalf("Failed json decode of array close token: %s", err)
-		}
-		csvWriter.Flush()
-		pipew.Close()
-	}()
-
-	return piper, nil
-}
-
-func (d *ExportData) GetRawReader() (io.ReadCloser, error) {
-	return gzip.NewReader(d.src)
 }
 
 func getFSClient(conf *config.Config) *fullstory.Client {
